aec4f382562a23de4e10e0c14acb8a76e09a743e
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-10-29 03:01:49 +0800

    5

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const request = require("request");
-console.log("Hello Xanxus");
+let http = require("http"); // 1 - 載入 Node.js 原生模組 http
+
+let server = http.createServer(function (req, res) {   // 2 - 建立server
+
+    // 在此處理 客戶端向 http server 發送過來的 req。
+    console.log("Hello Xanxus");
+
+});
+
+server.listen(process.env.PORT || 5000); //3 - 進入此網站的監聽 port, 就是 localhost:xxxx 的 xxxx
+
+console.log("Node.js web server at port 5000 is running..");
 // const options = {
 //     method: "GET",
 //     url: "https://irs.thsrc.com.tw/IMINT/",
